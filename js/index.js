500a1a10341702672cb0d115d8b41d063552dd67
wg1003141025 <1003141025@qq.com>
2017-12-18 23:41:29 +0800

    1

--- js/index.js
+++ js/index.js
@@ -24,6 +24,62 @@ window.onload = function () {
             banner_con[i].classList.add("cate_part_active");
         }
     }
+    //banner的轮播
+    let banner_bkuai = document.getElementsByClassName("ban_cen_top")[0];
+    let banner_tu = document.querySelectorAll(".bct_l");
+    let banner_dian = document.querySelectorAll(".yuan");
+    let now = 0;
+    let banner_time = setInterval(banner_t,1000);
+    let banner_left = document.getElementsByClassName("wang_left")[0];
+    let banner_right = document.getElementsByClassName("wang_right")[0];
+    function banner_t(can){
+        can = can || "right";
+        if(can == "right"){
+            now++;
+            if(now == banner_tu.length){
+                now = 0;
+            }
+        }else if(can == "left"){
+            now--;
+            if(now < 0){
+                now = banner_tu.length-1;
+            }
+        }
+
+        banner_tu.forEach(function (value,index) {
+            banner_dian[index].classList.remove("yuan_active");
+            value.classList.remove("bct_l_active");
+        });
+        banner_dian[now].classList.add("yuan_active");
+        banner_tu[now].classList.add("bct_l_active");
+        //now++;
+    }
+    banner_bkuai.onmouseover = function () {
+        clearInterval(banner_time);
+    }
+    banner_bkuai.onmouseout = function () {
+        banner_time = setInterval(banner_t,1000);
+    }
+    banner_dian.forEach(function (value1,index1) {
+        value1.onclick = function () {
+            banner_tu.forEach(function (value2,index2) {
+                value2.classList.remove("bct_l_active");
+                banner_dian[index2].classList.remove("yuan_active");
+            })
+            banner_tu[index1].classList.add("bct_l_active");
+            this.classList.add("yuan_active");
+            now = index1;
+        }
+    })
+    banner_left.onclick = function () {
+        banner_t("left");
+    }
+    banner_right.onclick = function () {
+        banner_t("right");
+    }
+
+
+
 
     //按需加载
     var agg = document.getElementsByClassName("ag");
